887e27169fb55d37c098166aa11bc1f8886a2361
Alexandre Rousseau <contact@rousseau-alexandre.fr>
2018-04-13 17:01:18 +0200

    Search words from french dict

--- src/main.rs
+++ src/main.rs
@@ -7,9 +7,13 @@ extern crate rocket;
 extern crate rocket_contrib;
 
 use rocket_contrib::Json;
+use std::io::{BufReader, BufRead};
+use std::fs::File;
 use std::path::Path;
 use rocket::response::NamedFile;
 
+
+const MAX_WORDS: i8 = 40;
 
 /// Home page
 #[get("/")]
@@ -20,13 +24,36 @@ pub fn home() -> Option<NamedFile> {
 /// JSON API
 #[get("/<word>", format = "application/json")]
 pub fn query(word: String) -> Option<Json> {
+    let mut words: Vec<String> = Vec::new();
+    let mut i = 0i8;
+
+    let file = File::open("/usr/share/dict/french").unwrap();
+    for line in BufReader::new(file).lines() {
+
+        if i > MAX_WORDS {
+            continue;
+        }
+
+        match line {
+            Ok(line_content) => {
+                if line_content.contains(&word) {
+                    words.push(str::replace(
+                        &line_content,
+                        &word,
+                        &format!("<strong>{}</strong>", word),
+                    ));
+                    i += 1;
+                }
+            }
+            Err(_) => {}
+
+        };
+    }
+
+
     // todo find word here
     Some(Json(json!({
-        "results": [
-            "First word",
-            "Lorem ipsum",
-            word,
-        ]
+        "results": words
     })))
 }
 
