57ea2fcf39566ddd07726cd632717a624d1c1708
Alexandre Rousseau <contact@rousseau-alexandre.fr>
2018-04-13 15:34:23 +0200

    Use vue.js

--- src/main.rs
+++ src/main.rs
@@ -7,33 +7,13 @@ extern crate rocket;
 extern crate rocket_contrib;
 
 use rocket_contrib::Template;
-use std::path::{Path, PathBuf};
-
-use rocket::response::NamedFile;
-
 use rocket_contrib::Json;
 
-/*
-#[derive(Serialize, Deserialize)]
-struct Word {
-    title: String,
-    description: String,
-}
-
-impl Word {
-    fn new(title: &str, description: &str) -> Word {
-        Word {
-            title: title.to_string(),
-            description: description.to_string(),
-        }
-    }
-}
-*/
 
 /// Home page
 #[get("/")]
 pub fn home() -> Template {
-    Template::render("pages/home", &{})
+    Template::render("home", &{})
 }
 
 /// JSON API
@@ -44,21 +24,15 @@ pub fn query(word: String) -> Option<Json> {
         "results": [
             "First word",
             "Lorem ipsum",
-            word
+            word,
         ]
     })))
-}
-
-/// Static files
-#[get("/assets/<file..>")]
-fn files(file: PathBuf) -> Option<NamedFile> {
-    NamedFile::open(Path::new("static/").join(file)).ok()
 }
 
 
 fn main() {
     rocket::ignite()
-        .mount("/", routes![home, files])
+        .mount("/", routes![home])
         .mount("/query", routes![query])
         .attach(Template::fairing())
         .launch();
