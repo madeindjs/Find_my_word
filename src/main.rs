a087b3c27065417b5fa680b8833c04f33a67d956
Alexandre Rousseau <contact@rousseau-alexandre.fr>
2018-04-18 13:14:54 +0200

    Read two local dictionnaries

--- src/main.rs
+++ src/main.rs
@@ -17,17 +17,25 @@ const MAX_WORDS: i8 = 40;
 
 /// Home page
 #[get("/")]
-pub fn home() -> Option<NamedFile> {
+fn home() -> Option<NamedFile> {
     NamedFile::open(Path::new("static/home.html")).ok()
 }
 
 /// JSON API
 #[get("/<word>", format = "application/json")]
-pub fn query(word: String) -> Option<Json> {
+fn query(word: String) -> Option<Json> {
+    Some(Json(json!({
+        "french": get_words("french", &word),
+        "english": get_words("english", &word)
+    })))
+}
+
+fn get_words(lang: &str, patern: &str) -> Vec<String> {
     let mut words: Vec<String> = Vec::new();
+
     let mut i = 0i8;
 
-    let file = File::open("/usr/share/dict/french").unwrap();
+    let file = File::open(format!("dict/{}", lang)).unwrap();
     for line in BufReader::new(file).lines() {
 
         if i > MAX_WORDS {
@@ -36,11 +44,11 @@ pub fn query(word: String) -> Option<Json> {
 
         match line {
             Ok(line_content) => {
-                if line_content.contains(&word) {
+                if line_content.contains(patern) {
                     words.push(str::replace(
                         &line_content,
-                        &word,
-                        &format!("<span class=\"text-primary\">{}</span>", word),
+                        patern,
+                        &format!("<span class=\"text-primary\">{}</span>", patern),
                     ));
                     i += 1;
                 }
@@ -50,11 +58,8 @@ pub fn query(word: String) -> Option<Json> {
         };
     }
 
+    return words;
 
-    // todo find word here
-    Some(Json(json!({
-        "results": words
-    })))
 }
 
 
